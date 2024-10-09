42e46308f9b82de15c18b0977dcf3e7c31b002d8
Ryan Roche <48164514+ryan-roche@users.noreply.github.com>
2024-10-09 15:14:22 -0500

    Update generate_manifest.py

--- scripts/generate_manifest.py
+++ scripts/generate_manifest.py
@@ -3,23 +3,35 @@ import json
 import subprocess
 from datetime import datetime
 
-def get_last_commit_date(path):
-    try:
-        # Get the last commit date for the files in the directory
-        last_commit_date = subprocess.check_output(
-            ['git', 'log', '-1', '--format=%cd', '--date=iso', path],
-            stderr=subprocess.STDOUT
-        ).strip().decode('utf-8')
-        return last_commit_date
-    except subprocess.CalledProcessError:
-        return None
+def get_last_commit_date_for_files(files, dirpath):
+    last_commit_date = None
+    
+    for file in files:
+        file_path = os.path.join(dirpath, file)
+        try:
+            # Get the last commit date for the individual file
+            commit_date = subprocess.check_output(
+                ['git', 'log', '-1', '--format=%cd', '--date=iso', file_path],
+                stderr=subprocess.STDOUT
+            ).strip().decode('utf-8')
+            
+            # Update last_commit_date to be the most recent of all the files
+            if last_commit_date is None or commit_date > last_commit_date:
+                last_commit_date = commit_date
+        except subprocess.CalledProcessError:
+            continue
+    
+    return last_commit_date
 
 def get_manifest_data(instructions_dir):
     manifest = {'instructions': {}}
 
     for dirpath, dirnames, filenames in os.walk(instructions_dir):
-        # Get the last commit date for the directory
-        last_modified = get_last_commit_date(dirpath)
+        if filenames:
+            # Get the last commit date based on the most recent change among the files
+            last_modified = get_last_commit_date_for_files(filenames, dirpath)
+        else:
+            last_modified = None
 
         # Extract the directory name from the path
         dir_name = os.path.basename(dirpath)
